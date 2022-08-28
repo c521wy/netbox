4602ba6ee1ff02e1292da85ddf98f48df21c7ed1
蔡伟强 <caiweiqiang@vip.qq.com>
2022-08-29 01:59:09 +0800

    fix: get remote real ip when using proxy

--- src/main/java/cn/c521wy/netbox/common/util/WebUtils.java
+++ src/main/java/cn/c521wy/netbox/common/util/WebUtils.java
@@ -1,10 +1,19 @@
 package cn.c521wy.netbox.common.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 import javax.servlet.http.HttpServletRequest;
 
 public class WebUtils {
 
     public static String getRemoteIp(HttpServletRequest request) {
+
+        String realIp = request.getHeader("X-Real-IP");
+
+        if (StringUtils.isNotBlank(realIp)) {
+            return realIp;
+        }
+
         return request.getRemoteAddr();
     }
 
